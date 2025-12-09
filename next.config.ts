06c29eced33b1e63c94215d9dea6caf87e78be05
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-12-09 17:05:29 +0530

    feat: configure remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.odishashop.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ritikart.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
